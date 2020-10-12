250cffcb51984b73477971e89abcfbd795ef5c42
Aleksandr Shafir <aleksandr.shafir.ipad@mail.ru>
2020-10-12 13:30:07 +0300

    initial 3

--- Sanbox/src/SandboxApp.cpp
+++ Sanbox/src/SandboxApp.cpp
@@ -1,6 +1,9 @@
 #include <Workbench.h>
 
-int main() {
+int main(int argc, char** argv) {
+	for (int i = 1; i < argc; i++) {
+		std::cout << argv[i] << "\n";
+	}
 	Workbench::Engine mEngine;
 	mEngine.Run();
 	return 0;
